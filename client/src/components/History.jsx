09ea1771688a858f3ae206cb8a242ee683bfabe5
GarrettF <grfoster24@gmail.com>
2021-08-22 10:22:10 -0400

    Trying to get workouts to render on History component, not working

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,16 +1,27 @@
-import Update from "./Update";
-import { Link, Route } from "react-router-dom";
+// import Update from "./Update";
+// import { Link, Route } from "react-router-dom";
 
 function History(props) {
+  const { workouts } = props;
   return (
     <>
       <h1>This is the history component</h1>
-      <Route path="/update">
+      {/* <Route path="/update">
         <Link to="/update">
-          <button>Update</button>
+        <button>Update</button>
         </Link>
         <Update />
-      </Route>
+      </Route> */}
+      {workouts.map((workout, index) => {
+        const { exercise, sets, reps, rest, weight } = workout.fields;
+        <article>
+          <p>{exercise}</p>
+          <p>{sets}</p>
+          <p>{reps}</p>
+          <p>{rest}</p>
+          <p>{weight}</p>
+        </article>;
+      })}
     </>
   );
 }
