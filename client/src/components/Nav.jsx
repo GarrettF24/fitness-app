19f34b125e63759753ca731194d034b6fba19502
GarrettF <grfoster24@gmail.com>
2021-08-23 19:21:23 -0400

    fixed nav links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 import "./Nav.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// // import { FaSortDown } from "@fortawesome/react-fontawesome";
-// import { FaSortDown } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 function Nav(props) {
   return (
     <>
       <nav className="dropdown">
         <h1>Fitness-App</h1>
-        <p>dropdown here</p>
-        <div class="dropdown-content">
-          <Link to="/">Home</Link>
-          <Link to="/history">History</Link>
+        <div className="test">
+          <FontAwesomeIcon icon={faCaretDown} />
+          <div class="dropdown-content">
+            <Link to="/">Home</Link>
+            <Link to="/history">History</Link>
+          </div>
         </div>
       </nav>
     </>
