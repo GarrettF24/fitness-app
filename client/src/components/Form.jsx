86d6c09e7c81a55631d8c8bc1fefa5b94f2df1c8
GarrettF <grfoster24@gmail.com>
2021-08-25 11:03:23 -0400

    netifly deploy

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -55,45 +55,55 @@ function Form(props) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="exercise">Exercise</label>
-      <input
-        id="exercise"
-        value={exercise}
-        type="text"
-        onChange={(e) => setExercise(e.target.value)}
-      />
-      <label htmlFor="sets">Sets</label>
-      <input
-        id="sets"
-        value={sets}
-        type="text"
-        onChange={(e) => setSets(e.target.value)}
-      />
-      <label htmlFor="reps">Reps</label>
-      <input
-        id="reps"
-        value={reps}
-        type="text"
-        onChange={(e) => setReps(e.target.value)}
-      />
-      <label htmlFor="rest">Rest</label>
-      <input
-        id="rest"
-        value={rest}
-        type="text"
-        onChange={(e) => setRest(e.target.value)}
-      />
-      <label htmlFor="weight">Weight</label>
-      <input
-        id="weight"
-        value={weight}
-        type="text"
-        onChange={(e) => setWeight(e.target.value)}
-      />
-      <button type="button">+</button>
-      <button type="submit">Create Workout</button>
-    </form>
+    <>
+      <div className="title">
+        <h1>Create a Workout</h1>
+      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        {/* <label htmlFor="exercise">Exercise</label> */}
+        <input
+          placeholder="Exercise"
+          id="exercise"
+          value={exercise}
+          type="text"
+          onChange={(e) => setExercise(e.target.value)}
+        />
+        {/* <label htmlFor="sets">Sets</label> */}
+        <input
+          placeholder="Sets"
+          id="sets"
+          value={sets}
+          type="text"
+          onChange={(e) => setSets(e.target.value)}
+        />
+        {/* <label htmlFor="reps">Reps</label> */}
+        <input
+          placeholder="Reps"
+          id="reps"
+          value={reps}
+          type="text"
+          onChange={(e) => setReps(e.target.value)}
+        />
+        {/* <label htmlFor="rest">Rest</label> */}
+        <input
+          placeholder="Rest"
+          id="rest"
+          value={rest}
+          type="text"
+          onChange={(e) => setRest(e.target.value)}
+        />
+        {/* <label htmlFor="weight">Weight</label> */}
+        <input
+          placeholder="Weight"
+          id="weight"
+          value={weight}
+          type="text"
+          onChange={(e) => setWeight(e.target.value)}
+        />
+        {/* <button type="button">+</button> */}
+        <button type="submit">Create Workout</button>
+      </form>
+    </>
   );
 }
 
